3e1f635cead50038a68271ba9cf4a35cbafd8a71
catherine-leonhard <catherinerleonhard@gmail.com>
2025-06-30 10:26:39 -0400

    artwork side navigation

--- scripts/page.js
+++ scripts/page.js
@@ -4,10 +4,14 @@ $(document).ready(function () {
   // SHOW/HIDE PROJECT OVERLAYS
   $('.open-project').click(function() {
     project_id = $(this).attr('id');
+    $('#projects').hide();
+    $('#art').hide();
     $('#' + project_id + '.overlay').show();
   })
 
   $('.close-project').click(function() {
+    $('#projects').show();
+    $('#art').show();
     $('#' + project_id + '.overlay').hide();
   })  
 });
