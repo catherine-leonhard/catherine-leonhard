dd8db4a71562f0ced5240cb92620287c426f67c2
catherine-leonhard <catherinerleonhard@gmail.com>
2025-09-29 19:11:03 -0400

    image resizing

--- scripts/page.js
+++ scripts/page.js
@@ -1,13 +1,20 @@
 let project_id;
+let scroll_position = 0;
 
 $(document).ready(function () {
   // SHOW/HIDE PROJECT OVERLAYS
   $('.open-project').click(function() {
     project_id = $(this).attr('id');
+    scroll_position = $(window).scrollTop();
+    $("#art").addClass("no-scroll").css({
+      top: -scroll_position + "px"              // lock body in place
+    });
     $('#' + project_id + '.overlay').show();
   })
 
   $('.close-project').click(function() {
+    $("#art").removeClass("no-scroll").css({ top: "" });
+    $(window).scrollTop(scroll_position);
     $('#' + project_id + '.overlay').hide();
   })  
 });
