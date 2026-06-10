b861277bf411efd6786e382ff11b769a411e9469
catherine-leonhard <catherinerleonhard@gmail.com>
2026-06-09 22:16:02 -0400

    redesign-6-9-26

--- scripts/page.js
+++ scripts/page.js
@@ -2,6 +2,37 @@ let project_id;
 let scroll_position = 0;
 
 $(document).ready(function () {
+
+  if (sessionStorage.getItem('splashSeen')) {
+      $('#splash').hide();
+  } 
+  else {
+    sessionStorage.setItem('splashSeen', 'true');
+
+    $('#splash').click(function() {
+      $(this).fadeOut('slow');
+      $('body').css('overflow', 'auto');  // re-enable scrolling
+    })
+}
+
+  // SPLASH SCREEN
+  $('#splash').click(function() {
+    $(this).fadeOut('slow');
+  })
+
+  $('#top-nav .icon').click(function() {
+    if ($("#nav-items").css('display') === 'none') {
+      $("#nav-items").css({
+        "display": "flex",
+      });    
+    }
+    else {
+      $("#nav-items").css({
+        "display": "none",
+      });
+    }
+  })
+
   // SHOW/HIDE PROJECT OVERLAYS
   $('.open-project').click(function() {
     project_id = $(this).attr('id');
@@ -16,5 +47,5 @@ $(document).ready(function () {
     $("#art").removeClass("no-scroll").css({ top: "" });
     $(window).scrollTop(scroll_position);
     $('#' + project_id + '.overlay').hide();
-  })  
+  }) 
 });
